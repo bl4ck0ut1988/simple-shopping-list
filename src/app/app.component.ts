ab53388d2c4412e17d1e386b26b5f224a92f5650
bl4ck0ut <kevin.mattmueller@gmail.com>
2018-09-03 19:42:52 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   title = 'simple-shopping-list';
   update = false;
 
+  // test
+
   constructor(swUpdate: SwUpdate) {
     swUpdate.available.subscribe(() => {});
   }
