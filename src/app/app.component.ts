5aca1434c0c4be4e49914e3a6e98f900256c0a2f
bl4ck0ut <kevin.mattmueller@gmail.com>
2018-09-04 17:36:51 +0200

    add functionality to manage a single list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { AngularFirestore, CollectionReference } from 'angularfire2/firestore';
+import { AngularFirestore, CollectionReference, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
+
+interface ShoppingList {
+  list: any[];
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'simple-shopping-list';
 
   itemName = '';
   quantity = null;
-  items: Observable<any[]>;
+
+  itemCollection: AngularFirestoreCollection<any>;
+  list: AngularFirestoreDocument<ShoppingList>;
+  temporaryItems: any[] = [];
   // test 123
 
   constructor(
-    private db: AngularFirestore,
     // private cr: CollectionReference,
+    private afs: AngularFirestore,
     private swUpdate: SwUpdate
-  ) {
-    swUpdate.available.subscribe(() => {});
-    this.items = db.collection('items').valueChanges();
+  ) {}
+
+  ngOnInit(): void {
+    this.swUpdate.available.subscribe(() => {});
+
+    this.itemCollection = this.afs.collection('lists');
+
+    // this.itemCollection = this.afs.collection('lists', ref => {
+    //   // use query on collection
+    //   return ref.where('name', '==', 'firstList');
+    // });
+
+    this.itemCollection.valueChanges().subscribe(lists => {
+      // this.items = lists[0].list;
+      if (lists.length >= 1) {
+        this.temporaryItems = lists[0].list;
+      } else {
+        this.temporaryItems = [];
+      }
+    });
+
+    this.list = this.afs.doc('lists/testList');
+
+    this.list.valueChanges().subscribe(list => {
+      this.temporaryItems = list.list;
+    });
   }
 
-  onSubmit(): void {
-    // this.db.collection('/items').push({ content: this.itemValue });
-    this.db.collection('items').add({name: this.itemName, quantity: this.quantity});
+  onAddItem(): void {
+    this.temporaryItems.push({ name: this.itemName, quantity: this.quantity });
     this.itemName = '';
     this.quantity = null;
   }
 
+  onSave(): void {
+    this.itemCollection.doc('testList').set({
+      list: this.temporaryItems,
+      name: 'testList'
+    });
+  }
+
   onDelete(): void {
-    // TODO: Delete collection
+    this.list = this.afs.doc('lists/testList');
+
+    if (this.list) {
+      this.temporaryItems = [];
+      this.list.delete();
+    }
   }
 }
