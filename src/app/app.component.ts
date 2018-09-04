789b72da6d41dd3e695ddfafecd1f275dd77dbfd
bl4ck0ut <kevin.mattmueller@gmail.com>
2018-09-04 15:32:42 +0200

    allow user to add items, let app fetch collection of items on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { AngularFirestore, CollectionReference } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +11,29 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class AppComponent {
   title = 'simple-shopping-list';
-  update = false;
 
+  itemName = '';
+  quantity = null;
+  items: Observable<any[]>;
   // test 123
 
-  constructor(swUpdate: SwUpdate) {
+  constructor(
+    private db: AngularFirestore,
+    // private cr: CollectionReference,
+    private swUpdate: SwUpdate
+  ) {
     swUpdate.available.subscribe(() => {});
+    this.items = db.collection('items').valueChanges();
+  }
+
+  onSubmit(): void {
+    // this.db.collection('/items').push({ content: this.itemValue });
+    this.db.collection('items').add({name: this.itemName, quantity: this.quantity});
+    this.itemName = '';
+    this.quantity = null;
+  }
+
+  onDelete(): void {
+    // TODO: Delete collection
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -12,9 +14,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
