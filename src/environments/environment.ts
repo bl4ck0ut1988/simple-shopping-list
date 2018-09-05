5814b9c7495bc1575cdf259a7964e3aa3b0eeb0c
bl4ck0ut <kevin.mattmueller@gmail.com>
2018-09-05 13:05:10 +0200

    add comments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDJF-5UJSQEj3-1wCxW04J5_aJ3PDFI0TU',
-    authDomain: 'simpleshoppinglist-ba6e8.firebaseapp.com',
-    databaseURL: 'https://simpleshoppinglist-ba6e8.firebaseio.com',
-    projectId: 'simpleshoppinglist-ba6e8',
-    storageBucket: 'simpleshoppinglist-ba6e8.appspot.com',
-    messagingSenderId: '444277130466'
-  }
+  firebase: {}
 };
 
 /*
